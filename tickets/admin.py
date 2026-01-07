ac6d3f2476aee3f4ab127986825e1532b9e16ded
Brandon-Lea Price <brandonlea810@gmail.com>
2026-01-07 10:50:32 +0000

    Add Order and OrderLineItem models for e-commerce

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Ticket
+from .models import Ticket, Order, OrderLineItem
 
 
 @admin.register(Ticket)
@@ -55,3 +55,43 @@ class TicketAdmin(admin.ModelAdmin):
         """Display percentage sold"""
         return f"{obj.percentage_sold:.1f}%"
     percentage_sold.short_description = '% Sold'
+
+
+class OrderLineItemInline(admin.TabularInline):
+    """Inline admin for OrderLineItem"""
+    model = OrderLineItem
+    readonly_fields = ['lineitem_total']
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """Admin configuration for Order model"""
+    inlines = [OrderLineItemInline]
+
+    list_display = [
+        'order_number',
+        'full_name',
+        'email',
+        'date',
+        'order_total',
+    ]
+    list_filter = ['date']
+    search_fields = ['order_number', 'full_name', 'email']
+    readonly_fields = ['order_number', 'date', 'order_total']
+
+    fieldsets = (
+        ('Order Information', {
+            'fields': ('order_number', 'date', 'user')
+        }),
+        ('Customer Details', {
+            'fields': ('full_name', 'email', 'phone_number')
+        }),
+        ('Payment', {
+            'fields': ('order_total', 'stripe_pid')
+        }),
+    )
+
+    def has_add_permission(self, request):
+        """Disable manual order creation in admin"""
+        return False
--- tickets/models.py
+++ tickets/models.py
@@ -1,7 +1,10 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
 from django.core.exceptions import ValidationError
 from django.utils import timezone
+from django.db.models import Sum
 from events.models import Event
 
 
@@ -102,3 +105,79 @@ class Ticket(models.Model):
         if self.quantity_available == 0:
             return 0
         return (self.quantity_sold / self.quantity_available) * 100
+
+
+class Order(models.Model):
+    """
+    Order model representing a customer's ticket purchase
+    """
+    order_number = models.CharField(max_length=32, unique=True, editable=False)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='orders'
+    )
+    full_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    phone_number = models.CharField(max_length=20)
+    date = models.DateTimeField(auto_now_add=True)
+    order_total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+    stripe_pid = models.CharField(max_length=254, default='')
+
+    class Meta:
+        ordering = ['-date']
+
+    def _generate_order_number(self):
+        """Generate a random, unique order number using UUID"""
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """Override save to set order number if not set"""
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def update_total(self):
+        """Update order total based on line items"""
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total')
+        )['lineitem_total__sum'] or 0
+        self.save()
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderLineItem(models.Model):
+    """
+    OrderLineItem model representing individual tickets in an order
+    """
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name='lineitems'
+    )
+    ticket = models.ForeignKey(
+        Ticket,
+        on_delete=models.CASCADE
+    )
+    quantity = models.PositiveIntegerField(default=1)
+    lineitem_total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        editable=False
+    )
+
+    def save(self, *args, **kwargs):
+        """Override save to set lineitem total and update order total"""
+        self.lineitem_total = self.ticket.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.ticket.ticket_type} for {self.ticket.event.title} (x{self.quantity})'
