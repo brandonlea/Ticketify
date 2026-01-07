15223e51652b66cf497d4fe0974bf6c632bc1a1c
Brandon-Lea Price <brandonlea810@gmail.com>
2026-01-07 10:48:05 +0000

    Configure Heroku deployment with Procfile and WhiteNoise

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Ticketify.wsgi:application
--- Ticketify/settings.py
+++ Ticketify/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,6 +127,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+# WhiteNoise configuration for serving static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (User uploaded content)
 MEDIA_URL = 'media/'
